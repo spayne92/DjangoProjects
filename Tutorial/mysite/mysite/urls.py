412ba07958bf70f8b271b65df1bb99ed45f27e17
Scott <spayne92@live.com>
2016-05-11 03:00:34 -0500

    Made it to third page of tutorial.

--- Tutorial/mysite/mysite/urls.py
+++ Tutorial/mysite/mysite/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 
+from . import views
+
 urlpatterns = [
+    # first param: refers to current page (home)
+    #second param: refers to view to be loaded (index)
+    # third param: assigns variable name to url path
+    url(r'^$', views.index, name='index'),
     url(r'^admin/', admin.site.urls),
+    url(r'^polls/', include('polls.urls')),
 ]
