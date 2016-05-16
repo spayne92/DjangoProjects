baa4964c5833b75dc8e7d065ab8ec7b14c5a7c73
Laptop <spayne92@live.com>
2016-05-16 17:29:53 -0500

    Finished part 4 of tutorial.

--- Tutorial/mysite/polls/views.py
+++ Tutorial/mysite/polls/views.py
@@ -1,25 +1,36 @@
 # Create your views here.
-from django.http import HttpResponse    # N/A
-from django.http import Http404
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.core.urlresolvers import reverse
 from django.template import loader      # N/A
+# shorctut objects to shorten idiomatic operations
 from django.shortcuts import get_object_or_404, render
+# shortcut for creating idiomatic generic views
+from django.views import generic
 
-from .models import Question
+from .models import Question, Choice
 
-def index(request):
+class IndexView(generic.ListView):
     # creates list of 5 most recent questions (by pub_date)
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #latest_question_list = Question.objects.order_by('-pub_date')[:5]
     # loads template for polls index page
-    # //template = loader.get_template('polls/index.html')
+    #template = loader.get_template('polls/index.html')
     # builds dictionary mapping to be passed to template
     # (maps template variable name to python object)
-    context = {'latest_question_list': latest_question_list}
+    #context = {'latest_question_list': latest_question_list}
     # returns template loaded with above context
-    # //return HttpResponse(template.render(context,request))
+    #return HttpResponse(template.render(context,request))
     # shortcut to skip template loading step
-    return render(request, 'polls/index.html', context)
+    #return render(request, 'polls/index.html', context)
 
-def detail(request, question_id):
+    # generic view version of above implemenations
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        # return last five published questions
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
     """
     # tries to assign object with question based on primKey
     try:
@@ -29,13 +40,36 @@ def detail(request, question_id):
         raise Http404("Question does not exist")
     """
     # compacts try/except from above version
-    question = get_object_or_404(Question, pk=question_id)
+    #question = get_object_or_404(Question, pk=question_id)
     # renders page with polls/detail template loaded with question context
-    return render(request, 'polls/detail.html', {'question': question})
+    #return render(request, 'polls/detail.html', {'question': question})
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    # generic view version of above implementations
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    #question = get_object_or_404(Question, pk=question_id)
+    #return render(request, 'polls/results.html', {'question': question})
+
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
